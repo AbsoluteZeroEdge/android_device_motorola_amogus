95e01530ea53290b8f9e9aeb2a6b4a359c83fbce
AbsoluteZeroEdge <hankefey@gmail.com>
2021-12-04 20:10:49 -0500

    Push Device Tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device.mk \
+    $(LOCAL_DIR)/aosp_amogus.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_amogus-user \
+    aosp_amogus-userdebug \
+    aosp_amogus-eng \
